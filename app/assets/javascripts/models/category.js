e14e0319681f46ba5345a48f442ccd4813de60aa
lily <lily@aa.io>
2015-05-14 11:39:09 -0400

    add category show

--- app/assets/javascripts/models/category.js
+++ app/assets/javascripts/models/category.js
@@ -1,3 +1,20 @@
 Leppo.Models.Category = Backbone.Model.extend({
-  urlRoot: '/api/categories'
+  urlRoot: '/api/categories',
+
+  posts: function () {
+    if (!this._posts) {
+      this._posts = new Leppo.Collections.Posts([], { collection: this });
+    }
+
+    return this._posts;
+  },
+
+  parse: function (response) {
+    if (response.posts) {
+      this.posts().set(response.posts, { parse: true });
+      delete response.posts;
+    }
+
+    return response;
+  }
 });
--- app/assets/javascripts/views/categories/category_show.js
+++ app/assets/javascripts/views/categories/category_show.js
@@ -1,5 +1,5 @@
 Leppo.Views.CategoryShow = Backbone.View.extend({
-  template: _.template("<h1>Category Show View</h1><h1><%= categories.escape('title') %></h1><%= categories.escape('body') %><a href='#/'>Index</a>"),
+  template: JST["categories/show"],
 
   initialize: function () {
     this.listenTo(this.model, 'sync', this.render);
@@ -7,8 +7,9 @@ Leppo.Views.CategoryShow = Backbone.View.extend({
 
   render: function () {
     var renderedContent = this.template({
-      categories: this.model
+      category: this.model
     });
+
     this.$el.html(renderedContent);
     return this;
   }
